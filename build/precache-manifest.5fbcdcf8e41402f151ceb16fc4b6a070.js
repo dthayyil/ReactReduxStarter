76ae0865e4c20c971a19f26eb49ffef34ef006f4
Deepak Thayyil <deepak@ejeevalive.onmicrosoft.com>
2020-03-24 10:47:16 +0530

    Build Folder

---
+++ build/precache-manifest.5fbcdcf8e41402f151ceb16fc4b6a070.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52b6b717670450fa69b7a246496644f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65ffac266f82b223bdcf",
+    "url": "/static/css/2.b53fa550.chunk.css"
+  },
+  {
+    "revision": "57f5dd65c258c605e271",
+    "url": "/static/css/main.7ae4160c.chunk.css"
+  },
+  {
+    "revision": "65ffac266f82b223bdcf",
+    "url": "/static/js/2.3c81bc6c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3c81bc6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57f5dd65c258c605e271",
+    "url": "/static/js/main.ede1be96.chunk.js"
+  },
+  {
+    "revision": "945334cb6c2d906b5ffb",
+    "url": "/static/js/runtime-main.e53a53ac.js"
+  }
+]);
